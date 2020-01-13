3b72d5a8a4c22ed3457d4e0516354f9d8be472e0
Alan Banks <alanbanks229@gmail.com>
2020-01-13 21:46:01 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+
+def run_guessing_game()
+  #rand_number = rand(1..6) I am very suprised this won't work
+  rand_number = rand(6) + 1
+  puts "Guess your own number between 1 and 6"
+  user_input = gets.chomp
+  if user_input.to_s == rand_number.to_s
+    puts "You guessed the correct number!"
+  elsif user_input == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{rand_number}."
+  end
+end
